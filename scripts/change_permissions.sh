b05aab54e6dc28c9c6e2a1ea18fd13f3dbb78448
Ankur Sahoo <Ankur.Sahoo@lloydsbanking.com>
2019-07-16 00:32:43 +0100

    updated scripts

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,1 +1,6 @@
-echo "inside change permissions"
+#!/bin/bash
+echo "******inside change permissions******"
+chmod 755 start_server.sh
+chmod 755 stop_server.sh
+chmod 755 game-0.1.0.jar
+echo "******done with change permissions******"
