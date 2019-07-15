30bd84004213138dd661cd1c8b5fb3f3e4b76443
Ankur Sahoo <Ankur.Sahoo@lloydsbanking.com>
2019-07-16 00:42:46 +0100

    updated paths

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "******inside change permissions******"
-chmod 755 start_server.sh
-chmod 755 stop_server.sh
-chmod 755 game-0.1.0.jar
+chmod 755 ./start_server.sh
+chmod 755 ./stop_server.sh
+chmod 755 ./game-0.1.0.jar
 echo "******done with change permissions******"
